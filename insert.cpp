20b3488e76a3be2d82db1c787398dcd24663a484
KhTimur <khazirov@yandex.ru>
2018-05-20 19:29:29 +0300

    Update insert.cpp

--- insert.cpp
+++ insert.cpp
@@ -3,14 +3,13 @@
 
 using namespace std;
 
-// Сортировка вставками
 
 void vec(int * begi, int *en)
 {
     for(int *ptr  = begi; ptr != en; ptr++)
         for(int *j=ptr; *j != *begi && *(j-1)>*j;j--)
-			{
-			   swap(*(j-1),*j);
+	    {
+	      swap(*(j-1),*j);
             }
 }
 
@@ -27,9 +26,7 @@ int main()
    int nums[] = { 2,9,3,1,5,7 };
     int *begi = std::begin(nums);
     int *en = end(nums);
-
     vec(begi, en);
-
     print(begi,en);
     return 0;
 }
