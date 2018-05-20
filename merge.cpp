6a2ac43efeac98f261d52756d82653f01efedc81
KhTimur <khazirov@yandex.ru>
2018-05-20 19:28:14 +0300

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -2,18 +2,54 @@
 #include <algorithm>
 using namespace std;
 
-//Слияние
 
+void inplmerg (int* begi, int* middle, int* en)
+{
+   int const siz = std::distance(begi, en);
+   int num[siz];
+   int k=0;
+   int j=0;
+   int i=0;
+   while( ((begi+i)<middle) && ((middle+k)<en)  )
+       if(*(begi+i) < *(middle+k))
+          {
+            num[j]=*(begi+i);
+            i++;
+            j++;
+          }
+       else
+          {
+            num[j]=*(middle+k);
+            k++;
+            j++;
+          }
+   while ((begi+i)<middle)
+        {
+            num[j]=*(begi+i);
+            i++;
+            j++;
+        }
+   while ((middle+k)<en)
+        {
+            num[j]=*(middle+k);
+            k++;
+            j++;
+        }
+   for (int i = 0; i<siz; i++)
+      {
+        *(begi+i)=num[i];
+      }
+}
 
 
 void merge_sort(int * begi, int *en)
 {
-    int const N = std::distance(begi, en);
-    if (N <= 1) return;
-    int * const middle = std::next(begi, N / 2);
+    int const siz = std::distance(begi, en);
+    if (siz <= 1) return;
+    int * const middle = std::next(begi, siz / 2);
     merge_sort(begi, middle);
     merge_sort(middle, en);
-    std::inplace_merge(begi, middle, en);
+    inplmerg(begi, middle, en);
 }
 
 void print(int *begin, int *end)
@@ -26,10 +62,9 @@ void print(int *begin, int *end)
 
 int main()
 {
-    int nums[] = { 2,9,3,1,5,7 };
+    int nums[] = { 9,3,2,13,7,5,1};
     int *begi = std::begin(nums);
     int *en = end(nums);
-
     merge_sort(begi, en);
     print(begi,en);
     return 0;
