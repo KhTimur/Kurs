4e2055a115c116e30b0dfb9963138d46e5d79918
KhTimur <khazirov@yandex.ru>
2018-06-12 22:42:27 +0300

    blabla

---
+++ tmp/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gwaji/Kurs/sources/insert.cpp" "/home/gwaji/Kurs/tmp/CMakeFiles/lib.dir/sources/insert.cpp.o"
+  "/home/gwaji/Kurs/sources/merge.cpp" "/home/gwaji/Kurs/tmp/CMakeFiles/lib.dir/sources/merge.cpp.o"
+  "/home/gwaji/Kurs/sources/piramyd.cpp" "/home/gwaji/Kurs/tmp/CMakeFiles/lib.dir/sources/piramyd.cpp.o"
+  "/home/gwaji/Kurs/sources/quick.cpp" "/home/gwaji/Kurs/tmp/CMakeFiles/lib.dir/sources/quick.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ tmp/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/sources/insert.cpp.o"
+  "CMakeFiles/lib.dir/sources/merge.cpp.o"
+  "CMakeFiles/lib.dir/sources/piramyd.cpp.o"
+  "CMakeFiles/lib.dir/sources/quick.cpp.o"
+  "liblib.pdb"
+  "liblib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
